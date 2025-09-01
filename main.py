72446f360cbc9eca306fcc54c4d45be4f33b9a4d
John Lindsay <jwlindsay17@gmail.com>
2025-08-31 22:43:41 -0600

    20250831: Updated Stats and Main for dictionaries of characters. Chapter 3, Lesson 1 of bootdev

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
-from stats import get_num_words, get_char_counts
-
+from stats import get_num_words, get_char_counts, get_sorted_chars
+# from stats import print_sorted_chars
 
 def get_book_text(path_to_file):
-    with open(path_to_file) as f:
+   with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
 def main(path_to_file):
-    print(get_num_words(get_book_text(path_to_file)))
-    print(get_char_counts(get_book_text(path_to_file)))    
+    book_text = get_book_text(path_to_file)
+    num_words = get_num_words(book_text)
+    char_counts = get_char_counts(book_text)
+    sorted_char_list = get_sorted_chars(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------") 
+    for pair in sorted_char_list:
+        print(f"{pair['char']}: {pair['num']}")
 
-main("./books/frankenstein.txt")
+main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def get_num_words(file_contents):
     word_list = file_contents.split()
-    return f"{len(word_list)} words found in the document"
+    return len(word_list)
 
 def get_char_counts(file_contents):
     characters = file_contents.lower()
@@ -11,3 +11,18 @@ def get_char_counts(file_contents):
         else:
             char_count[char] = 1
     return char_count
+
+def sort_on(char_count):
+    return char_count["num"]
+
+def get_sorted_chars(char_count):
+    sorted_chars = []
+    for letter in char_count:
+        count = char_count[letter]
+        sorted_chars.append({"char":letter, "num":count})
+    sorted_chars.sort(reverse=True, key=sort_on)
+    return sorted_chars
+
+# def print_sorted_chars(sorted_chars):
+#     for pair in sorted_chars:
+#         print(f"{pair["char"]}: {pair["num"]}")
